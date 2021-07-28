ab082813e2356269c73bda78e35245c858dffae3
shemlevy <shem@codeocean.com>
2021-07-28 21:08:55 +0300

    delete dist

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./use-scroll-to-element.cjs.production.min.js')
-} else {
-  module.exports = require('./use-scroll-to-element.cjs.development.js')
-}
